456b7ea002794432753c8f4464fbb1274c33b7f8
Lubomir I. Ivanov <neolit123@gmail.com>
2025-01-07 00:13:04 +0200

    add SHA256 to build.cmd

--- build.cmd
+++ build.cmd
@@ -13,5 +13,8 @@ go-winres simply --icon .\assets\charging_3.ico --manifest gui
 echo building...
 go build -ldflags "-s -H=windowsgui -X=main.version=%BUILD_VERSION%"
 
+echo generating SHA256...
+sha256sum go-dualsense-battery.exe > go-dualsense-battery.exe.sha256
+
 set BUILD_VERSION=
 set CGO_ENABLED=%OLD_CGO_ENABLED%
